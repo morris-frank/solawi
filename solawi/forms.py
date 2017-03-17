d426a1713f97f9914b13123020889e66cae72dac
Tukkin Moriarty <mrtukkin@gmail.com>
2017-03-17 20:25:58 +0100

    Added weeklybasktform

--- solawi/forms.py
+++ solawi/forms.py
@@ -1,5 +1,22 @@
 from django import forms
+from solawi.models import (
+    OrderBasket,
+    WeeklyBasket,
+)
 
 
-class WeeklyOrderForm(forms.Form):
-    pass
+class WeeklyBasketForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['contents'].queryset = kwargs.get('instance').contents
+
+    class Meta:
+        model = WeeklyBasket
+        fields = ['contents']
+        widgets = {'contents': forms.CheckboxSelectMultiple()}
+
+
+class OrderBasketFrom(forms.ModelForm):
+    class Meta:
+        model = OrderBasket
+        fields = ['contents']
--- solawi/views.py
+++ solawi/views.py
@@ -2,7 +2,15 @@ import datetime
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views import generic
-from solawi.models import Depot, Portion, Product, User, WeeklyBasket, OrderBasket
+from solawi import forms
+from solawi.models import (
+    Depot,
+    OrderBasket,
+    Portion,
+    Product,
+    User,
+    WeeklyBasket,
+)
 from solawi import utils
 
 
@@ -14,13 +22,22 @@ def gemuesekorb(request):
 def woche(request, year=None, week=None):
     date = utils.date_from_week(year, week)
     user = request.user
-    try:
-        orders = user.orders.get(week=date)
-    except OrderBasket.DoesNotExist:
-        orders = None
+
+    weeklybasketform = None
+    if user.weeklybasket:
+        weeklybasketform = forms.WeeklyBasketForm(instance=user.weeklybasket)
+
+    orders = user.orders.filter(week=date)
+    orderbasketform = None
+    if orders:
+        orders = orders[0]
+        orderbasketform = forms.OrderBasketFrom(instance=orders)
+
     portions_list = Portion.objects.order_by('-quantity')
     context = {'user': user,
                'orders': orders,
+               'weekly_basket_form': weeklybasketform,
+               'order_basket_form': orderbasketform,
                'portions_list': portions_list,
                'controls': {'next_week': date + datetime.timedelta(7),
                             'prev_week': date - datetime.timedelta(7)}}
